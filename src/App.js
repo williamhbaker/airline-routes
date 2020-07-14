190a8accbb736935c3643f7e6149f45736234e18
William Baker <wbaker@gmail.com>
2020-07-14 11:45:53 -0400

    raw route data table

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import React from 'react';
 import './App.sass';
 import './App.css';
 
-function App() {
-  return (
-    <section className="hero is-primary">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">
-            Airline Routes
-          </h1>
-        </div>
-      </div>
-    </section>
-  );
-}
+import Table from './Table.js';
 
-export default App;
+import airlineData from './data.js';
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <>
+        <section className="hero is-primary">
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <h1 className="title">
+                Airline Routes
+              </h1>
+            </div>
+          </div>
+        </section>
+        <section className="section">
+          <div className="container">
+            <Table
+              routes={airlineData.routes}
+            />
+          </div>
+        </section>
+      </>
+    );
+  }
+}
---
+++ src/Table.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function Table(props) {
+  return (
+    <table className="table is-striped is-fullwidth" style={{margin: '0 auto'}}>
+      <thead>
+        <tr>
+          <th>Airline</th>
+          <th>Source Airport</th>
+          <th>Destination Airport</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.routes.map((route) => {
+          return (
+            <tr key={route}>
+              <td>{route.airline}</td>
+              <td>{route.src}</td>
+              <td>{route.dest}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+Table.propTypes = {
+  routes: propTypes.array.isRequired,
+};
+
+export default Table;
