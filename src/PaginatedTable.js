cab386577e3441bef6ba0bdd1aedea6edff5e891
William Baker <wbaker@gmail.com>
2020-07-15 12:20:02 -0400

    converted to functional component

--- src/PaginatedTable.js
+++ src/PaginatedTable.js
@@ -4,47 +4,40 @@ import propTypes from 'prop-types';
 import Table from './Table.js';
 import Paginator from './Paginator.js';
 
+function PaginatedTable(props) {
+  const maxPages = Math.ceil(props.rows.length / props.maxRows);
+  const startingRow = (props.currentPage - 1) * props.maxRows;
+  const boundedLimit = props.currentPage * props.maxRows;
+  const endingRow = boundedLimit < props.rows.length ? boundedLimit : props.rows.length;
+  const visibleRows = props.rows.slice(startingRow, endingRow);
 
-class PaginatedTable extends React.Component {
-  static propTypes = {
-    format: propTypes.func.isRequired,
-    columns: propTypes.array.isRequired,
-    rows: propTypes.array.isRequired,
-    currentPage: propTypes.number.isRequired,
-    onPageClick: propTypes.func.isRequired,
-    maxRows: propTypes.number.isRequired,
-  };
+  return (
+    <section className="section" style={{paddingTop: '1rem'}}>
+      <div className="container">
+        <Table
+          format={props.format}
+          columns={props.columns}
+          rows={visibleRows}
+        />
+        <Paginator
+          maxPages={maxPages}
+          currentPage={props.currentPage}
+          onPageClick={props.onPageClick}
+        />
 
-  maxPages = () => Math.ceil(this.props.rows.length / this.props.maxRows);
-
-  startingRow = () => (this.props.currentPage - 1) * this.props.maxRows;
-
-  endingRow = () => {
-    const boundedLimit = this.props.currentPage * this.props.maxRows;
-    return boundedLimit < this.props.rows.length ? boundedLimit : this.props.rows.length;
-  }
-
-  visibleRows = () => this.props.rows.slice(this.startingRow(), this.endingRow());
-
-  render() {
-    return (
-      <section className="section" style={{paddingTop: '1rem'}}>
-        <div className="container">
-          <Table
-            format={this.props.format}
-            columns={this.props.columns}
-            rows={this.visibleRows()}
-          />
-          <Paginator
-            maxPages={this.maxPages()}
-            currentPage={this.props.currentPage}
-            onPageClick={this.props.onPageClick}
-          />
-          <p className='has-text-centered'>{`Showing ${this.startingRow() + 1}-${this.endingRow()} of ${this.props.rows.length} records.`}</p>
-        </div>
-      </section>
-    );
-  }
+        <p className='has-text-centered'>{`Showing ${startingRow + 1}-${endingRow} of ${props.rows.length} records.`}</p>
+      </div>
+    </section>
+  );
 }
+
+PaginatedTable.propTypes = {
+  format: propTypes.func.isRequired,
+  columns: propTypes.array.isRequired,
+  rows: propTypes.array.isRequired,
+  currentPage: propTypes.number.isRequired,
+  onPageClick: propTypes.func.isRequired,
+  maxRows: propTypes.number.isRequired,
+};
 
 export default PaginatedTable;
